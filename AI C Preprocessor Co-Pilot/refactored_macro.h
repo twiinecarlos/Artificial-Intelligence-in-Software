8876eecbe30e5fa3bda42353369096142e1c707e
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-14 19:44:02 +0400

    Update refactored_macro.h

--- AI C Preprocessor Co-Pilot/refactored_macro.h
+++ AI C Preprocessor Co-Pilot/refactored_macro.h
@@ -1,1 +1,19 @@
+#ifndef REFACTORED_MACRO_H
+#define REFACTORED_MACRO_H
 
+#include <stdio.h>
+
+#define MULTIPLY(a, b) ((a) * (b))
+
+#ifdef DEBUG
+#define DEBUG_LOG(msg) \
+    do { \
+        fprintf(stderr, "[DEBUG] %s:%d: %s\n", __FILE__, __LINE__, msg); \
+    } while (0)
+#else
+#define DEBUG_LOG(msg) \
+    do { \
+    } while (0)
+#endif
+
+#endif /* REFACTORED_MACRO_H */
